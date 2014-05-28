fad8fff9a54585baf7ab9f4c348f0e7d2dc26dd7
cjord01 <clayjchicago@gmail.com>
2014-05-28 18:56:45 -0500

    3.6 - Release 0

--- week_3/6_group_project/gcl_user_stories.js
+++ week_3/6_group_project/gcl_user_stories.js
@@ -2,10 +2,14 @@
 
 // Here is my part of the challenge:
 
-// I want to...
-
-	// Start with two set of numbers
-		//The first group of numbers is 1, 2, 3, 4, 5, 5, 7
-		//The second group of numbers is 4, 4, 5, 5, 6, 6, 6, 7
-
-	//
+//I want to take two sets of numbers and find the total sum, mean, and median for each set of numbers. 
+	//The above is really all of the information you should need but 
+	// I've included some additional text just incase you want more 
+	// clarification, but it's pretty much restating the above. 
+		//I have two sets of numbers.
+			//The first has an odd number of values. (this likely won't be important to you)
+			//The second has an even number of values. (this likely won't be important to you)
+		//I need to do three things to each set of numbers.
+			//1. I need to add up the value of all the numbers in each set.
+			//2. I need to find the mean (or average) of each set of numbers.
+			//3. I need to find the median of each set of numbers.  
