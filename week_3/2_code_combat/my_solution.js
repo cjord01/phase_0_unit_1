095b46b693ac0f7ff982f8d19156961bd42ff2b5
cjord01 <clayjchicago@gmail.com>
2014-05-28 15:02:41 -0500

    3.2 My Solutions

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,121 @@
-// I worked on this challenge [by myself, with:]
+// I worked on this challenge by myself:
  
 // For each mission, write the title as a comment. (Shown here). Also include pseudocode as a comment. 
 // Note: to make commenting easier, you can highlight the section you want to comment and hold 
 // command + / This will comment the line. 
  
+// 1. Rescue Mission
+	//delete "this.moveRight()"
+	//move up x2
+	//move right x2
+	//move down
+	//attack!
+
+// 2. Grab the Mushroom
+	// move up
+	// move right
+	//move left
+	//move up
+	//attack!
+
+	// this.moveUp();
+	// this.moveRight();
+	// this.moveLeft();
+	// this.moveUp();
+	// this.attackNearbyEnemy();
+
+// 3. Drink Me
+
+	// attack
+	// move right
+	// move down
+	// move up
+	// move right
+	// attack
+
+	// this.attackNearbyEnemy();
+	// this.moveRight();
+	// this.moveDown();
+	// this.moveUp();
+	// this.moveRight();
+	// this.attackNearbyEnemy();
+
+// 4. Taunt the 
+
+	// move right x2
+	// move up
+	// move right
+	// say something
+	// move down
+	// move right
+	// move up
+	// move right
+
+	// this.moveRight();
+	// this.moveRight();
+	// this.moveUp();
+	// this.moveRight();
+	// this.say("Hey guard!");
+	// this.moveDown();
+	// this.moveRight();
+	// this.moveUp();
+	// this.moveRight();
+
+// 5. It's a Trap
+
+	// move down x2
+	// say something
+	// move up x2
+
+	// this.moveDown();
+	// this.moveDown();
+	// this.say("Yo!");
+	// this.moveUp();
+	// this.moveUp();
+
+// 6. Break the Prison
+
+	// if(name === "William")
+	//     return true;
+	// if(name === "Krogg")
+	//     return false;  // <-- Start here; Krogg is not a friend!
+	// if(name === "Brack")
+	//     return false;
+	// if(name === "Gort")
+	//     return false;
+	// if(name == "MARCUS")
+	//     return false;
+
+	// return true;
  
- 
- 
- 
- 
- 
- 
- 
+// 7. Taunt
+
+	// say three things
+
+	// this.say("you're ugly");
+	// this.say("I don't like your skin tone");
+	// this.say("you probably don't smell very good");
+
+// 8. Cowarly Taunt
+
+	// this.moveXY(50, 16);
+	// this.say("I can lure them in here.");
+	// this.moveXY(70, 10);  
+
+// 9. Commanding Followers
+
+	// this.say("Follow!");
+	// this.moveXY(66, 48);
+	// this.say("Attack!");
+
+// 10. Mobile Artillery
+
+	// this.moveXY(54, 40);
+	// this.attackXY(69, 56);
+	// this.attackXY(65, 50);
+	// this.attackXY(48,66);
+	// this.attackXY(51, 54);
+
+
 // Reflection:
 // Write your reflection here.
---
+++ week_3/6_group_project/gcl_user_stories.js
@@ -0,0 +1,11 @@
+// My role in the group is: Person 1
+
+// Here is my part of the challenge:
+
+// I want to...
+
+	// Start with two set of numbers
+		//The first group of numbers is 1, 2, 3, 4, 5, 5, 7
+		//The second group of numbers is 4, 4, 5, 5, 6, 6, 6, 7
+
+	//
