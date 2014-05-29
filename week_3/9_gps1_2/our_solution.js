5bd29147c4a63a946266f696a8332dd0e1eb7eff
cjord01 <clayjchicago@gmail.com>
2014-05-28 23:16:22 -0500

    3.9 - Solutions & Reflections

--- week_3/9_gps1_2/our_solution.js
+++ week_3/9_gps1_2/our_solution.js
@@ -1,45 +1,32 @@
 // GPS 1.2 - JavaScript
 
 // YOUR FULL NAMES:
-//  1.
-//  2.
+//  1. Clayton Jordan
+//  2. Steve Pikelny
 
+function Client(name, age, quote) {
+    this.name = name;
+    this.age = age;
+    this.quote = quote;
+    this.showQuote = function() {
+        console.log("My client, " + this.name + " is " + this.age + '-years-old. Most known for the quote: "' + this.quote + '"')
+    }
+}
 
-// 0. "YOU SIGNED... WHO?!"
+var adamSandler = new Client("Adam Sandler", 47, "That's your home! Are you too good for your home?!")
+adamSandler.showQuote();
 
+var kristenBell = new Client("Kristen Bell", 33, "Do you wanna build a snowman?")
 
+var jimCarrey = new Client ("Jim Carrey", 52, "...So you're telling me there's a chance? YEAH!")
 
+console.log("My client, " + adamSandler.name + " is " + adamSandler.age + "-years-old. He's known for the quote: " +'"'+adamSandler.quote+'"')
+console.log("My client, " + kristenBell.name + " is " + kristenBell.age + "-years-old. She's known for the quote: " +'"'+kristenBell.quote+'"')
+console.log("My client, " + jimCarrey.name + " is " + jimCarrey.age + "-years-old. He's known for the quote: " +'"'+jimCarrey.quote+'"')
 
-// 1. "Here they Come!"
-
-
-
-
-
-// 2. "TIME IS MONEY!"
-
-//YOUR CODE HERE!
-
-var shooterMcGavin = new Client("Shooter McGavin", 48, "Just stay out of my way... or you'll pay. Listen to what I say.");
-shooterMcGavin.constructor === Client;
-shooterMcGavin.age === 48;
-shooterMcGavin.quote === "Just stay out of my way... or you'll pay. Listen to what I say.";
-
-
-
-
-
-// 3. "SHOW 'EM OFF!"
-
-
-
-
-
-// 4. "But wait, there's more!"
-
-
-
-// 5.  ** BONUS **
-
+adamSandler.showQuote()
 
 //  6.  Your Reflection:
+// Johnny said that we did better than any of his other groups but I felt a bit like Steve carried be a little through this session. He seemed to be 
+// ahead of me with this stuff. This is a little frustrating because I'm nearly done with my challenges this week so it seems like I should know this stuff
+// better than most. JS feels a bit over my head. I am curious to see how it's going to tie in with HTML and CSS. 
