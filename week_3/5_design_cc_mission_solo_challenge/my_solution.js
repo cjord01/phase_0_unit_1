ef70c7d7745b0986679b3ef27c6825f44fd911b9
cjord01 <clayjchicago@gmail.com>
2014-05-30 13:36:33 -0500

    3.5 - Challenge & Reflection

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,37 +1,37 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
+// Navigate the maze, pick up the magic strength cream, 
+// then attack the bad guy. 
+
+//this.moveLeft();
+//this.moveRight();
+//this.moveUp();
+//this.moveDown();
+//this.attackBadGuy();
 
 // Pseudocode
 // 
-// 
-// 
-// 
-// 
+// move right
+// move down
+// move right x2
+// move left
+// move up x2
+// attack!
 
 // Initial Code
 
-
-
-
-
-
-// Refactored Code
-
-
-
-
-
+this.moveRight();
+this.moveDown();
+this.moveRight();
+this.moveRight();
+this.moveLeft();
+this.moveUp();
+this.moveUp();
+this.attackBadGuy();
 
 // Reflection
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
+// I feel like I must be missing something here but my understanding
+// of the directions is that I'm recreating a mission similar to
+// what we did in Challenge 2. I hope that the above satifies the
+// requirements of the Challenge. 
